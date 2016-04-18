36a67aeec73a29a479fe2eb55bcb538c97976622
Laure Philips <lphilips@soft.vub.ac.be>
2016-04-18 18:24:52 +0200

    small bug fixes

--- stip/transpiler/JSify.js
+++ stip/transpiler/JSify.js
@@ -546,7 +546,7 @@ var JSify = (function () {
 
     function transformNewExp (transpiler) {
         var node        = transpiler.node,
-            upnode      = node.getInNodes(EDGES.DATA)[0],
+            upnode      = node.getInNodes(EDGES.DATA).concat(node.getInNodes(EDGES.CONTROL).filter(function (n) {return n.isActualPNode}))[0],
             call        = upnode.getOutNodes(EDGES.CONTROL)
                             .filter(function (n) {return n.isCallNode && n.parsenode.equals(node.parsenode) })[0],
             parsenode   = node.parsenode,
@@ -554,15 +554,23 @@ var JSify = (function () {
             actual_outs = call.getActualOut();
 
         transpiler.nodes = transpiler.nodes.remove(call);
+
         actual_outs.map(function (a_out) {
             if (nodesContains(transpiler.nodes, a_out)) {
                 transpiler.nodes = transpiler.nodes.remove(a_out);
             }
         });
 
+        actual_ins.map(function (a_in) {
+            a_in.getOutNodes(EDGES.CONTROL)
+                .map(function (n) {
+                    transpiler.nodes = transpiler.nodes.remove(n);
+                });   
+        });
+
         parsenode.arguments = actual_ins.filter(function (a_in) {
             return nodesContains(transpiler.nodes, a_in);
-        }).map(function (a_in) { return a_in.parsenode; });
+        }).map(function (a_in) { transpiler.nodes = transpiler.nodes.remove(a_in); return a_in.parsenode; });
 
         transpiler.nodes = transpiler.nodes.remove(node);
         transpiler.transpiledNode = parsenode;
